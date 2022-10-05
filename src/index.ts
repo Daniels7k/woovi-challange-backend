20be0ab3a4ab2b7e94e4ef5d7f5d4261ac1f4c4b
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-05 07:11:50 -0300

    mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+import "dotenv/config"
 import express from "express";
 import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
+import mongoose from "mongoose";
+
 
 const main = async () => {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
+
+  mongoose.connect(`${process.env.MONGO_URI}`, (error) => {
+    if(error){
+        console.log(error)
+        return
+    }
+
+    console.log("Mongo Connected")
+  })
 
   // app.use("/graphql", graphqlHTTP({
   //     schema,
