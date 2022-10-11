a6af37a4becd20ad54a59ffc6fd9c5c808c90626
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-11 15:02:53 -0300

    despesa crud

---
+++ src/schema/mutations/despesa.ts
@@ -0,0 +1,47 @@
+import { GraphQLFloat, GraphQLID, GraphQLInt, GraphQLString } from "graphql";
+import { DespesaType } from "../typeDefs/despesa";
+
+import Despesa from "../../models/Despesa";
+
+export const CREATE_DESPESA = {
+  type: DespesaType,
+  args: {
+    name: { type: GraphQLString },
+    createdAt: { type: GraphQLString },
+    category: { type: GraphQLString },
+    value: { type: GraphQLFloat },
+  },
+  async resolve(parent: any, args: any) {
+    const despesa = new Despesa(args);
+    await despesa.save();
+
+    return args;
+  },
+};
+
+export const UPDATE_DESPESA = {
+  type: DespesaType,
+  args: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    createdAt: { type: GraphQLString },
+    category: { type: GraphQLString },
+    value: { type: GraphQLInt },
+  },
+  async resolve(parent: any, args: any) {
+    await Despesa.findByIdAndUpdate(args);
+
+    return args;
+  },
+};
+
+export const DELETE_DESPESA = {
+  type: DespesaType,
+  args: {
+    id: { type: GraphQLInt },
+  },
+  async resolve(parent: any, args: any) {
+    await Despesa.findByIdAndDelete(args.id);
+    return args;
+  },
+};
