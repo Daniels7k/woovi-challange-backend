e2d536b76e5ac6c28b51c37d2ef53a3e27eb5d59
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-11 15:04:22 -0300

    setting despesas in graphql index

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,12 +1,19 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import {
+  CREATE_DESPESA,
+  DELETE_DESPESA,
+  UPDATE_DESPESA,
+} from "./mutations/despesa";
 
 import { CREATE_USER } from "./mutations/user";
+import { DESPESAS } from "./queries/despesa";
 import { GET_ALL_USERS } from "./queries/user";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
     getAllUsers: GET_ALL_USERS,
+    despesas: DESPESAS,
   },
 });
 
@@ -14,6 +21,9 @@ const Mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
     createUser: CREATE_USER,
+    createDespesa: CREATE_DESPESA,
+    updateDespesa: UPDATE_DESPESA,
+    deleteDespesa: DELETE_DESPESA,
   },
 });
 
